155ebebd198e83369c575c4ba29c3d4e38f88dd8
brittleru <diemetal851@gmail.com>
2022-01-07 14:58:52 +0200

    tdd-by-example

---
+++ tdd-example/src/main/java/com/example/Foo.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Foo {
+
+
+    public String getBar() {
+        return "FooBar";
+    }
+}
---
+++ tdd-example/src/test/java/com/example/FooTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class FooTest {
+
+    @Test
+    void getBar() {
+
+        Foo foo = new Foo();
+
+        String result = foo.getBar();
+
+        assertEquals("FooBar", result);
+
+    }
+}
